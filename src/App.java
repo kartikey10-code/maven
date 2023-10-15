73b246041416988d51b429619e661a46ccd0f3cd
kartikey10-code <kartikeynarayan1998@outlook.com>
2023-10-15 16:01:47 +0530

    version 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Helllo, World!");
+        System.out.println("hello kartik");
     }
 }
