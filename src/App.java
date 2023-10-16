a45e4a38bf0982ff11cb1928d3e69079d0a08c87
kartikey10-code <kartikeynarayan1998@outlook.com>
2023-10-16 12:28:18 +0530

    v 6

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 public class App {
+    /**
+     * @param args
+     */
     public static void main(String[] args)  {
         System.out.println("Helllo, World!");
         System.out.println("hello kartik");
         System.out.println("this is test project ");
+        System.out.println("hello everyone");
     }
 }
