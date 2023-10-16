4fb72b86655aca50f621ec2930ddb79141b922c8
kartikey10-code <kartikeynarayan1998@outlook.com>
2023-10-16 13:04:51 +0530

    v 7

---
+++ src/logic.java
@@ -0,0 +1,7 @@
+public class logic {
+    public static void main(String[] args)  {
+        System.out.println("Helllo, World!");
+        System.out.println("hello ashwini");
+        System.out.println("this is test project ");
+        System.out.println("hello to  everyone");
+}
